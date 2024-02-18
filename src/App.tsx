4d0647949373091b851e0c818045ea462e84072d
Igor Silakov <igor.silakov85@gmail.com>
2024-02-18 01:01:05 +0100

    make my own pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+const About = lazy(() => delayForDemo(import('./About')));
+const Home = lazy(() => import('./Home'));
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => (
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+      </Routes>
+    </Suspense>
+  </div>
+);
+
+function delayForDemo(promise: Promise<typeof import("./About")>) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  }).then(() => promise);
 }
 
 export default App;
