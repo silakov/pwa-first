437147b624a85bd4eda33d2ee8e198c971d1cae3
Igor Silakov <igor.silakov85@gmail.com>
2024-02-20 13:12:20 +0100

    trying to fix problem with build

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
-import React, { lazy, Suspense } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React from "react";
 import SpeechToTextButton from "./SpeechToText";
-const About = lazy(() => delayForDemo(import('./About')));
-const Home = lazy(() => import('./Home'));
 
 const App: React.FC = () => (
   <div style={{width: '200px', margin: '100px auto', textAlign: 'center'}}>
-    {/* <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-    <p>serviceWorkerRegistration.register</p>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-      </Routes>
-    </Suspense> */}
     <SpeechToTextButton />
   </div>
 );
-
-function delayForDemo(promise: Promise<typeof import("./About")>) {
-  return new Promise(resolve => {
-    setTimeout(resolve, 2000);
-  }).then(() => promise);
-}
 
 export default App;
--- src/SpeechToText.tsx
+++ src/SpeechToText.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const SpeechToTextButton: React.FC = () => {
