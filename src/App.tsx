faab2acd3f14b6cf4a93d08b9fcda69d474cf5d1
Igor Silakov <igor.silakov85@gmail.com>
2024-03-18 17:46:36 +0100

    try some menu styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,11 @@ import './App.css'
 
 const App: React.FC = () => (
   <div className="wrapper">
-    <div className="header">
-      <AdSense.Google
-        client='ca-pub-1224813934765676'
-        slot='1'
-        style={{ display: 'block', width: '300px', height: '100px' }}
-      />
-    </div>
+    <AdSense.Google
+      client='ca-pub-1224813934765676'
+      slot='1'
+      style={{ display: 'block', width: '300px', height: '100px' }}
+    />
     <main className="main">
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
@@ -34,16 +32,26 @@ const App: React.FC = () => (
           <Link to="/">home</Link>
         </li>
         <li>
-          <Link to="/multiplications">times table</Link>
+          <span>cards</span>
+          <ul>
+            <li>
+              <Link to="/multiplications">times table</Link>
+            </li>
+            <li>
+              <Link to="/english">english</Link>
+            </li>
+            <li>
+              <Link to="/frontend">frontend</Link>
+            </li>
+          </ul>
         </li>
         <li>
-          <Link to="/english">english</Link>
-        </li>
-        <li>
-          <Link to="/frontend">frontend</Link>
-        </li>
-        <li>
-          <Link to="/upload">upload</Link>
+          <span>options</span>
+          <ul>
+            <li>
+              <Link to="/upload">upload</Link>
+            </li>
+          </ul>
         </li>
       </ul>
     </nav>
